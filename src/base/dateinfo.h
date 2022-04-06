545f8d3a2af2f51af9469f4abe71b567db9dba03
alpheratz0 <alpheratz99@protonmail.com>
2022-04-06 19:20:11 -0300

    refactor: improve include guard

--- src/base/dateinfo.h
+++ src/base/dateinfo.h
@@ -1,18 +1,18 @@
-#ifndef __XCALENDAR_DATEINFO_H__
-#define __XCALENDAR_DATEINFO_H__
+#ifndef __XCALENDAR_BASE_DATEINFO_H__
+#define __XCALENDAR_BASE_DATEINFO_H__
 
 #include "../util/numdef.h"
 
-struct dateinfo {
-	u32     day;
-	u32     month;
-	u32     year;
-	u32     firstday_weekday;
-	u32     num_days_in_month;
-	char    month_name[20];
-};
-
 typedef struct dateinfo dateinfo_t;
+
+struct dateinfo {
+	u32 day;
+	u32 month;
+	u32 year;
+	u32 firstday_weekday;
+	u32 num_days_in_month;
+	char month_name[20];
+};
 
 extern dateinfo_t
 dateinfo_from_today(void);
