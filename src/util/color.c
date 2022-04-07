80e1a7a5bf330e60c294f32a379377fca0963c93
alpheratz0 <alpheratz99@protonmail.com>
2022-04-06 23:20:29 -0300

    refactor: extract repetitive code to its own function

--- src/util/color.c
+++ src/util/color.c
@@ -1,19 +1,16 @@
 #include "numdef.h"
 #include "color.h"
 
+static u8
+blerp(u8 from, u8 to, u8 v) {
+	return from + ((to - from) * v) / 0xff;
+}
+
 extern u32
 color_lerp(u32 from, u32 to, u8 v) {
-	u8 rfrom = (from >> 16) & 0xff;
-	u8 rto   = (to >> 16) & 0xff;
-	u8 r     = rfrom + ((rto - rfrom) * v) / 0xff;
-
-	u8 gfrom = (from >> 8) & 0xff;
-	u8 gto   = (to >> 8) & 0xff;
-	u8 g     = gfrom + ((gto - gfrom) * v) / 0xff;
-
-	u8 bfrom = from & 0xff;
-	u8 bto   = to & 0xff;
-	u8 b     = bfrom + ((bto - bfrom) * v) / 0xff;
+	u8 r = blerp((from >> 16) & 0xff, (to >> 16) & 0xff, v);
+	u8 g = blerp((from >> 8) & 0xff, (to >> 8) & 0xff, v);
+	u8 b = blerp(from & 0xff, to & 0xff, v);
 
 	return (r << 16) | (g << 8) | b;
 }
