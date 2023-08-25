8d0a3503c9487c29e4ff591a9dc01db370097957
alpheratz0 <alpheratz99@protonmail.com>
2023-08-25 19:29:32 -0300

    format config.mk

--- config.mk
+++ config.mk
@@ -1,14 +1,12 @@
 # Copyright (C) 2022-2023 <alpheratz99@protonmail.com>
 # This program is free software.
 
-VERSION   = 0.3.16
-
-CC        = cc
-INCS      = -I/usr/include/freetype2 -I/usr/X11R6/include
-CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
-LDLIBS    = -lxcb -lfreetype -lxcb-image -lxcb-keysyms -lfontconfig
-LDFLAGS   = -L/usr/X11R6/lib -s
-
-PREFIX    = /usr/local
-MANPREFIX = $(PREFIX)/share/man
-APPPREFIX = $(PREFIX)/share/applications
+VERSION=0.3.16
+CC=cc
+INCS=-I/usr/include/freetype2 -I/usr/X11R6/include
+CFLAGS=-std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
+LDLIBS=-lxcb -lfreetype -lxcb-image -lxcb-keysyms -lfontconfig
+LDFLAGS=-L/usr/X11R6/lib -s
+PREFIX=/usr/local
+MANPREFIX=$(PREFIX)/share/man
+APPPREFIX=$(PREFIX)/share/applications
